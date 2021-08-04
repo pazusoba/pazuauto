b4fe4839f6ff79b3d183431f14889af4435dbb62
Yiheng <development.henryquan@gmail.com>
2021-08-04 22:46:19 +1000

    sort is working but pazusoba is not
    so the string is still incorrect?

--- cdll/pazusoba.py
+++ cdll/pazusoba.py
@@ -2,19 +2,37 @@
 Call methods exported in the shared library
 """
 from ctypes import *
+from typing import List
 import os
+
+
+class Route(Structure):
+    _fields_ = [("x", c_int),
+                ("y", c_int),
+                ("size", c_int)]
+
 
 # mac only for now, need to rename to libpazuosba.so
 libpazusoba = CDLL("../build/pazusoba/libpazusoba.dylib", winmode=0)
 
-libpazusoba.solve.restype = POINTER(c_int)
-libpazusoba.solve.argtypes = (POINTER(c_int), c_int)
+libpazusoba.solve.restype = None
+libpazusoba.solve.argtypes = (c_int, POINTER(POINTER(c_char)))
 
 
-def solve(argc, argv: list):
-    pass
+def solve(arguments: List[str]) -> Route:
+    byte_argv = bytearray()
+    size = 0
+    for s in arguments:
+        b = bytearray(s, 'utf-8')
+        byte_argv += b
+        size += len(b)
+    print(byte_argv)
+    argv = (c_char * (size + 1))(*byte_argv)
+    argc: int = len(arguments) + 1
+    return libpazusoba.solve(argc, argv)
 
 
 if __name__ == '__main__':
-    route = solve(4, ["RHLBDGPRHDRJPJRHHJGRDRHLGLPHBB", "3", "50", "10000"])
+    route = solve(
+        ["pazusoba", "RHLBDGPRHDRJPJRHHJGRDRHLGLPHBB", "3", "50", "10000"])
     print(route)
--- cdll/sort.py
+++ cdll/sort.py
@@ -1,4 +1,5 @@
 from ctypes import *
+from typing import List
 import os
 
 # https://stackoverflow.com/a/64472088
@@ -9,7 +10,8 @@ libsort = CDLL("./build/libsort.so", winmode=0)
 libsort.sortArray.restype = POINTER(c_int)
 libsort.sortArray.argtypes = (POINTER(c_int), c_int)
 
-def sort_array(array) -> list:
+
+def sort_array(array) -> List[int]:
     size = len(array)
     C_INT_ARRAY = (c_int * size)
     # convert to C int array with size
@@ -19,7 +21,9 @@ def sort_array(array) -> list:
     print(c_pointer[:size])
     return list(c_array)
 
+
 if __name__ == '__main__':
-    array = [123, 54, 213, 5, 93, 956, 370, 58, 164, 38, 69, 1000, 20000, 35321, 58719325, 21438021]
+    array = [123, 54, 213, 5, 93, 956, 370, 58, 164,
+             38, 69, 1000, 20000, 35321, 58719325, 21438021]
     sorted_array = sort_array(array)
     print(sorted_array)
