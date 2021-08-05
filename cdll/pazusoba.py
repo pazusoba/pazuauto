39e8486ef3fc806b17cd88722f9ee98edaf6e640
Yiheng <development.henryquan@gmail.com>
2021-08-05 23:04:29 +1000

    read data from C++ and free it properly

--- cdll/pazusoba.py
+++ cdll/pazusoba.py
@@ -6,33 +6,68 @@ from typing import List
 import os
 
 
-class Route(Structure):
+class CRoute(Structure):
     _fields_ = [("x", c_int),
                 ("y", c_int),
                 ("size", c_int)]
 
 
-# mac only for now, need to rename to libpazuosba.so
-libpazusoba = CDLL("../build/pazusoba/libpazusoba.dylib", winmode=0)
-libpazusoba.solve.restype = Route
+class Route:
+    """
+    Convert C Route to Python Object. 
+    NOTE: not necessary, use if this doesn't affect the performance
+    """
+    x = 0
+    y = 0
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({},{})".format(self.x, self.y)
+
+    def __repr__(self):
+        return "({},{})".format(self.x, self.y)
+
+
+libpazusoba = CDLL("../automation/pazusoba.so", winmode=0)
+# NOTE: the restype here must be correct to call it properly
+libpazusoba.solve.restype = POINTER(CRoute)
 libpazusoba.solve.argtypes = (c_int, POINTER(c_char_p))
-# have a look, https://stackoverflow.com/a/24061473
+libpazusoba.freeRoute.restype = None
+libpazusoba.freeRoute.argtypes = [POINTER(CRoute)]
 
 
-def solve(arguments: List[str]) -> Route:
+def solve(arguments: List[str]) -> List[Route]:
     # additional step is required here because Mac is stricter than Windows
     argv = []
     for s in arguments:
         argv.append(c_char_p(s.encode('ascii')))
         # argv.append(create_string_buffer(s.encode('ascii')))
-    print(argv)
     c_argc = len(arguments)
     c_argv = (c_char_p * c_argc)()
     c_argv[:] = argv
-    print(c_argv._objects)
-    return libpazusoba.solve(c_argc, c_argv)
+
+    routes_cpp = libpazusoba.solve(c_argc, c_argv)
+    if routes_cpp is None:
+        return []
+    else:
+        size = routes_cpp[0].size
+        routes = []
+        for i in range(size):
+            r = routes_cpp[i]
+            routes.append(Route(r.x, r.y))
+        freeList(routes_cpp)
+        return routes
+
+
+def freeList(routes: POINTER(CRoute)):
+    libpazusoba.freeRoute(routes)
 
 
 if __name__ == '__main__':
-    route = solve(["", "RHLBDGPRHDRJPJRHHJGRDRHLGLPHBB", "3", "50", "10000"])
-    print(route)
+    route = solve(
+        ["pazusoba.so", "RHLBDGPRHDRJPJRHHJGRDRHLGLPHBB", "3", "50", "10000"])
+    length = len(route)
+    print("There are {} steps.\n{}".format(length, route))
