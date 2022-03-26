eb86e34127e6a13455ff250b566b7bb737bd0d9d
Yiheng <development.henryquan@gmail.com>
2022-03-26 13:15:01 +1100

    added version in requirements and moved files to src

--- automation/config.py
+++ automation/config.py
@@ -2,56 +2,54 @@
 Configurations for the automation (static and dynamic)
 """
 
-# TODO: Everything should be moved to a class instead of using globals 
-# They are all captical so it is easy to extract them from methods
+class SingletonMeta(type):
+    """
+    Referenced from https://refactoring.guru/design-patterns/singleton/python/example#example-0
+    """
 
-# # # # # # # # # # # #
-#
-# CONSTANTS
-# Modify them before running
-#
-# # # # # # # # # # # #
+    _instances = {}
 
-# The cycle of automation
-ONE_CYCLE = 500
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
-# read location from game.loc
-import os
-if os.path.exists('game.loc'):
-    with open('game.loc', 'r') as loc:
-        last_location = eval(loc.read())
+class Config(metaclass=SingletonMeta):
+    # When in DEBUG mode, more texts will be printed
+    debug_mode = True
 
-    (game, board) = last_location
-    if (game is None) or (board is None):
-        print('Failed to reading locations')
-        exit(-1)
-    else:
-        GAME_LOCATION  = game
-        BOARD_LOCATION = board
-else:
-    print("Setup game location first with location.py")
-    exit(-1)
+    # The loop & screenshot cycle of automation in milliseconds
+    one_cycle_ms = 500
+    
+    # The location of the mirrored board
+    game_location = None
+    board_location = None
 
-# When in DEBUG mode, more texts will be printed
-DEBUG_MODE = True
+    # These values shouldn't be changed
+    orb_template_size = (140, 140)
+    board_uniform_size = (830, 690)
 
-# These values shouldn't be changed
-ORB_TEMPLATE_SIZE = (140, 140)
-BOARD_UNIFORM_SIZE = (830, 690)
+    # Resize for game ratio 2:1, 16:9 and 3:2
+    game_screen_size_2_1  = (1000, 1950)
+    game_screen_size_16_9 = (1000, 1720)
+    game_screen_size_3_2  = (1000, 1440)
 
-# Resize for game ratio 2:1, 16:9 and 3:2
-GAME_SCREEN_SIZE_2_1  = (1000, 1950)
-GAME_SCREEN_SIZE_16_9 = (1000, 1720)
-GAME_SCREEN_SIZE_3_2  = (1000, 1440)
+    # On Mac OS, the scale might be 2 instead of 1 because of the retina display
+    screen_scale = 1
 
-# On Mac OS, the scale might be 2 instead of 1 because of the retina display
-SCREEN_SCALE = 1
+    # Added paddings to images
+    boarder_length = 1
 
-# Added paddings to images
-BORDER_LENGTH = 1
+    # This is used to ignore similar matches by how close they are using this offset
+    sort_offset = 100
 
-# This is used to ignore similar matches by how close they are using this offset
-SORT_OFFSET = 100
+    # 20, 30 or 42
+    orb_count = 30
 
-# 20, 30 or 42
-ORB_COUNT = 30
+
+if __name__ == "__main__":
+    c1 = Config()
+    c2 = Config()
+    assert(id(c1) == id(c2))
+    
